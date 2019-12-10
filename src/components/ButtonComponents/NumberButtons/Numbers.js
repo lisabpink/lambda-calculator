511e15852b44c23b13b84cb6bbb850700a0f5224
LISA_COOK <lisabpink@gmail.com>
2019-12-10 16:32:53 -0500

    progress

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import NumberButton from './NumberButton';
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
 import { numbers } from '../../../data' 
 */
-//Import your array data to from the provided data file
 
-const Numbers = () => {
+//Import your array data to from the provided data file
+import { numbers } from '../../../data'
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
   return (
     <div>
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  console.log(props);
   return (
     <>
+    <button>
+        {props.text}
+      </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
